876aaadf251ab2ba453f812ba063dabab09e951b
ryu <m.joynauth@gmail.com>
2016-05-18 19:00:42 +0400

    WCF Architecture

---
+++ AdventureWorkService/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorkService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ProductService)))
+            {
+                host.Open();
+                Console.WriteLine("Server is Open");
+                Console.WriteLine("Press enter to close server");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WCFArchitecture/Program.cs
@@ -0,0 +1,29 @@
+﻿using AdventureWorkService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFArchitecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IProductService> channelFactory = new 
+                ChannelFactory<IProductService>("ProductServiceEndpoint");
+
+            IProductService proxy = channelFactory.CreateChannel();
+
+            List<string> products = proxy.ListProducts();
+
+            foreach (var p in products)
+            {
+                Console.WriteLine(p);
+            }
+            Console.ReadLine();
+        }
+    }
+}
